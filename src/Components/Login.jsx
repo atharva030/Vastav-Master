85fee45f2b5984547cae7b480c0f9ef0cfee06cd
Atharva Jagdale <91479983+atharva030@users.noreply.github.com>
2022-10-28 17:16:27 +0530

    Update Login.jsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -12,7 +12,7 @@ const Login = (props) => {
     const handleSubmit=async(e)=>{
        e.preventDefault();
        const response = await fetch(
-        `http://localhost:5000/api/auth/login`,
+        `https://atharva-jagdale.herokuapp.com/api/auth/login`,
         {
           method: "POST", // *GET, POST, PUT, DELETE, etc.
           headers: {
