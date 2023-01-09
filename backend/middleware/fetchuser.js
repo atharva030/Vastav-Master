e774eb0481ec01c8764e1a4067332ef63f81f74d
atharva030 <atharvas_jagdale@auton.in>
2023-01-09 14:41:55 +0530

    mess

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -2,7 +2,7 @@ var jwt = require("jsonwebtoken");
 const jwtSecretKey = process.env.JWT_SECRET_KEY || "atharva$$!oy";
 
 const fetchuser=(req,res,next)=>{
-    //get user from JWT_TOKEN
+    //get user from JWT_TOKEN   
 const token=req.header('auth-token');//this will sennd the request to header using the name auth-token for getting the token 
  if(!token){
     res.status(401).send({error:"Please Authenticate using valid token"})
