fff2386c77ebb7de93e7fb054f3e9a5d8ff42234
atharva030 <atharvas_jagdale@auton.in>
2023-02-08 13:55:50 +0530

    extra

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(express.json());
 //Available Routes
 app.use('/api/auth',require('./routes/auth'))
-app.use('/api/notes',require('./routes/notes'))
+// app.use('/api/notes',require('./routes/notes'))
   
 mongoose.connect(mongoURI,connectionParams).then(()=>{
     console.info("connected")
