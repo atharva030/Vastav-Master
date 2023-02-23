fc57806992f77f8872ece03ea3829a4333a613d8
atharva030 <atharvas_jagdale@auton.in>
2023-02-23 19:17:06 +0530

    Revert "spell"
    This reverts commit 0f40e738d302074b6ef2a6b85a6436e3b5871420.

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = ({ openModal }) => {
         <div className="leftnav-box">
           <div className="nav-links">
             <Link className="logo" to="/">
-              Vastav
+              Vatsav
             </Link>
           </div>
           <div className="nav-flex-text-image">
